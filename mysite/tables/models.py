363971d6eab4093078ed4ecd313ca5b1ce6be45d
sllachod <sllachod@unsa.edu.pe>
2024-06-13 08:10:21 -0500

    creando archivo urls.py e implementando el archivo models.py como el video

--- mysite/tables/models.py
+++ mysite/tables/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Simple(models.Model):
+    test = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
+    def __str__(self):
+        return self.url
+
+class  DataExample(models.Model):
+    the_date = models.DataTimeField()
+
+class NUllExample(models.Model):
+    col = models.CharField(max_length=10, blank=True, null=True)
