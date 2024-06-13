222894403339156a1dd25a5144a8188968e42f2b
sllachod <sllachod@unsa.edu.pe>
2024-06-13 09:11:43 -0500

    se hico migrate y se indento para imprmir pdf,no se soluciono

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],#"DIRS": [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
