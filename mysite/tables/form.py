19d80c71d31bbeda62cd4d7c93af6882ddf92d5d
sllachod <sllachod@unsa.edu.pe>
2024-06-15 22:24:52 -0500

    autenticando la contraseña del remitente para verificar su seguridad

--- mysite/tables/form.py
+++ mysite/tables/form.py
@@ -8,6 +8,7 @@ class NombreURLForm(forms.ModelForm):
     
 class EmailForm(forms.Form):
     remitente = forms.EmailField(label='Tu correo electrónico')
+    password = forms.CharField(widget=forms.PasswordInput(), label='Contraseña de correo electrónico')
     destinatario = forms.EmailField(label='Correo electrónico del destinatario')
     asunto = forms.CharField(max_length=100)
     mensaje = forms.CharField(widget=forms.Textarea)
