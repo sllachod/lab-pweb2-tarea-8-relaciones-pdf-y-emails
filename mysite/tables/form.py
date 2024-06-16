7f956064d90069cb6384a63f01642f9c3cf04153
sllachod <sllachod@unsa.edu.pe>
2024-06-15 22:03:53 -0500

    se crean un forms para el envio de emails

--- mysite/tables/form.py
+++ mysite/tables/form.py
@@ -5,3 +5,8 @@ class NombreURLForm(forms.ModelForm):
     class Meta:
         model = NombreURL
         fields = ['nombre', 'url']
+    
+class EmailForm(forms.Form):
+    destinatario = forms.EmailField(label='Correo electrónico del destinatario')
+    asunto = forms.CharField(max_length=100)
+    mensaje = forms.CharField(widget=forms.Textarea)
